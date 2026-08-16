312c906a87991196dd721562f6e3a1e2b86597fb
Mario Aprilnino <mario.aprilnino27@gmail.com>
2026-08-16 12:57:36 +0700

    chore: update formula to v0.3.19

--- Formula/lightcode.rb
+++ Formula/lightcode.rb
@@ -4,13 +4,13 @@ class Lightcode < Formula
   license "MIT"
 
   on_arm do
-    url "https://github.com/marioapn3/lightcode/releases/download/v0.3.18/lightcode-darwin-aarch64"
-    sha256 "d033e3e8ac6e04514b2d6ee8e1c5308a4e84e791338d8feab2371db8300605ed"
+    url "https://github.com/marioapn3/lightcode/releases/download/v0.3.19/lightcode-darwin-aarch64"
+    sha256 "79760337bd2592683d05673190bc14625ac26c9147d206f6eebd2dbe91123a41"
   end
 
   on_intel do
-    url "https://github.com/marioapn3/lightcode/archive/refs/tags/v0.3.18.tar.gz"
-    sha256 "87d7418a7b1398ffa8271e5ecd572c4cb39048d09ff4a8a28c489b05c77393d4"
+    url "https://github.com/marioapn3/lightcode/archive/refs/tags/v0.3.19.tar.gz"
+    sha256 "844aca1ca61564afaa82b0a35b6cdd1035438ff9b86354d5250ecc4e3f8d8479"
     depends_on "rust" => :build
   end
 
